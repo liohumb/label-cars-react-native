136a326aefb66ff445e00de41d1d0fa4067a80ad
liohumb <helloliohumb@gmail.com>
2023-01-05 16:25:45 +0100

    mise en place de la gestion de filtre des véhicules

--- components/utils/index.js
+++
@@ -1,9 +0,0 @@
-export function compare( a, b ) {
-    if ( a.reservations < b.reservations ){
-        return -1;
-    }
-    if ( a.reservations > b.reservations ){
-        return 1;
-    }
-    return 0;
-}
