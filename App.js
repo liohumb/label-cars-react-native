9f845731facd618cf09705da594d66634c36f003
liohumb <helloliohumb@gmail.com>
2023-02-21 21:54:36 +0100

    update project avant ajout de mongodb

--- App.js
+++ App.js
@@ -1,37 +1,48 @@
-import { Button } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { StatusBar } from 'expo-status-bar'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/Home';
+import { LogoTitle } from './src/LogoTitle';
+import { StatusBar } from 'expo-status-bar';
+import CarsList from './src/CarsList';
+import CarItem from './src/CarItem'
 
-import Home from './pages/home/Home'
-import Cars from './pages/cars/Cars'
-import Car from './pages/car/Car'
-import Logo from './components/logo/Logo'
+const Stack = createNativeStackNavigator();
 
-const Stack = createNativeStackNavigator()
-export default function App() {
-    return (
-        <NavigationContainer>
-            <StatusBar style="light"/>
-            <Stack.Navigator screenOptions={{
-                headerTintColor: '#fff',
-                headerStyle: {
-                    backgroundColor: '#2D4F6D'
-                },
-                headerBackTitle: 'retour'
-            }}>
-                <Stack.Screen name="Accueil"
-                              component={Home}
-                              options={{ headerTitle: () => <Logo/> }}/>
-                <Stack.Screen name="Nos véhicules"
-                              component={Cars}
-                              options={() => ({
-                                  headerRight: () => <Button title="Filtrer" color="#fff"/>
-                              })}/>
-                <Stack.Screen name="Voiture"
-                              component={Car}
-                              options={( { route } ) => ({ title: route.params.item.name })}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar style="light" />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#2D4F6C',
+          },
+          headerTintColor: '#fff',
+          headerTitleAlign: "center"
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerTitle: () => <LogoTitle /> }}
+        />
+        <Stack.Screen
+          name="CarsList"
+          component={CarsList}
+          options={{
+            headerTitle: "Nos véhicules",
+            headerBackTitle: "Accueil"
+          }}
+        />
+        <Stack.Screen
+          name="CarItem"
+          component={CarItem}
+          options={({ route }) => ({ title: route.params.item.name, headerBackTitle: "Retour" })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App
